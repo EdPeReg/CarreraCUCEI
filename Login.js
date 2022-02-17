b10916010c6be04c3561165d07ddde4156e1bf0f
EdPeReg <dardo1818@gmail.com>
2022-02-16 21:54:27 -0600

    #4 Buttons and input added
    Buttons for enviar, registrar and input for codigo and passwerd were added

--- Login.js
+++ Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // TouchableOpacity nos permite onclick, es transparente.
-import { View, Text, ImageBackground, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, Dimensions, TouchableOpacity, Image } from 'react-native';
 import { NavigationContext } from '@react-navigation/native';
+import { Input, Icon } from 'react-native-elements';
 
 export default class Login extends Component {
   static contextType = NavigationContext;
@@ -13,6 +14,7 @@ export default class Login extends Component {
 
   render() {
     const navigation = this.context;
+
     const btnLogin = () => {
         // Mostrara en la consola este mensaje.
         console.log("Preisonado")
@@ -22,13 +24,54 @@ export default class Login extends Component {
     return (
       <View>
         {/* Poner aqui la imagne de telegram del profe */}
-        <ImageBackground style={styles.bg} source={require("./Imagenes/login.png")}>
-            <View style={styles.btn}>
-                {/* Posicionar correctamente en el boton, llamara una funcion que se llama login. */}
-                <TouchableOpacity onPress={btnLogin}>
-                    {/* A fuerza ocupa haber un elemento, este es en blanco. */}
-                    <Text></Text>
-                </TouchableOpacity>
+        <ImageBackground style={styles.bg} source={require("./Imagenes/login_modified.png")}>
+          <View style={styles.input}>
+            {/* User field. */}
+            <Input
+                placeholder='Codigo'
+                placeholderTextColor='white'
+                leftIcon={
+                  <Icon 
+                    type='font-awesome'
+                    name='user-circle'
+                    size={24}
+                    color='white'
+                  />
+                }
+            />
+          </View>
+
+          <View>
+            {/* Pass field */}
+            <Input
+                placeholder='Password'
+                placeholderTextColor='white'
+                leftIcon={
+                  <Icon
+                    type='font-awesome'
+                    name='lock'
+                    size={24}
+                    color='white'
+                  />
+                }
+            />
+          </View>
+      
+
+            {/* Allows us to put a button side by side. */}
+            <View style={{ flexDirection:"row" }}>
+
+              {/* Enviar button */}
+              <Image style={styles.btn_send} source={require("./Imagenes/btn_send.png")} />
+
+              {/* Posicionar correctamente en el boton, llamara una funcion que se llama login. */}
+              <TouchableOpacity onPress={btnLogin}>
+                  {/* Registrarse button */}
+                  <Image style={styles.btn_reg} source={require("./Imagenes/btn_register.png")} />
+      
+                  {/* A fuerza ocupa haber un elemento, este es en blanco. */}
+                  <Text></Text>
+              </TouchableOpacity>
             </View>
         </ImageBackground>
       </View>
@@ -36,18 +79,35 @@ export default class Login extends Component {
   }
 }
 
+const win = Dimensions.get('window');
+const ratio = win.width / 1080;
+
 // Los estilos afectan cada uno de los elementos graficos.
 const styles = StyleSheet.create({
     bg: {
-        width: Dimensions.get("screen").width,
-        height: Dimensions.get("screen").height
+        width: win.width,
+        height: 2045 * ratio
     },
-    btn:{
+
+    btn_send:{
         borderWidth:3,
         borderColor: "white",
-        marginTop: 520,
-        marginLeft: Dimensions.get("screen").width/3.5,
+        marginTop: 80,
+        marginLeft: 50,
         width:150,
         height:40
-    }
+    },
+
+    btn_reg: {
+        borderColor: "white",
+        marginTop: 80,
+        marginLeft: 30,
+        width:150,
+        height:40
+    },
+    
+    // This is only to get the correct margin for the inputs.
+    input:{
+      marginTop: 480,
+    },
 })
